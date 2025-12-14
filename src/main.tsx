1b36e171b521e2b963c06557204de90acd5845f7
18120525 <thanhquang11220@gmail.com>
2025-12-14 10:39:06 +0700

    feat: handle toggle change dark - light mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/styles/global.css'
 import App from '@/app.tsx'
+import { ThemeProvider } from '@/components/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light" storageKey="movies-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
